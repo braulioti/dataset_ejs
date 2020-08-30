4c9379c97bbe0eccc059f352370fdc369e96123b
Bráulio <braulio@braulioti.com.br>
2020-08-30 09:11:48 -0300

    Parte 2 - Criando a estrutura de banco de dados do projeto

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,36 @@
 import * as express from 'express';
 import * as path from 'path';
+// https://node-postgres.com/
+import {Client} from 'pg';
 
 const app = express();
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'dataset_ejs',
+    password: 'postgres',
+    port: 5432
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
 
 app.get('/', (req, res) => {
+    // res.send('<h1>Hello Pesquisa!</h1>');
     res.render('index.ejs');
 });
 
-app.get('/pesquisa', (req, res) => {
-    res.render('pesquisa.ejs');
-    // res.send('<h1>Hello Pesquisa!</h1>');
-});
-
-app.get('/aprovacao', (req, res) => {
-    res.render('aprovacao.ejs');
+app.post('/aprovado', async (req, res) => {
+    try {
+        // throw new Error('Teste de erro!');
+        await client.connect();
+        const data = await client.query('SELECT * FROM obesity WHERE entity <> $1;', ['World']);
+        console.log(data.rows);
+        res.render('aprovado.ejs');
+    } catch (e) {
+        console.log(e.message);
+        res.render('erro-aprovacao.ejs');
+    }
 });
 
 app.listen(3000, () => {
